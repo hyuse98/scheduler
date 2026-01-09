eb3778e4d52fe3f5bc6850d496fbed5629863f83
Franklin <franklinpereira98@hotmail.com>
2026-01-09 18:05:07 -0300

    Add User Jpa Entity

---
+++ src/main/java/com/hyuse98/scheduler/iam/infrastructure/persistence/jpa/entity/UserJpaEntity.java
@@ -0,0 +1,83 @@
+package com.hyuse98.scheduler.iam.infrastructure.persistence.jpa.entity;
+
+import jakarta.persistence.*;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users", schema = "iam")
+public class UserJpaEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "email", unique = true, nullable = false)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "user_roles",schema = "iam",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<RoleJpaEntity> roles = new HashSet<>();
+
+    private boolean enabled = true;
+
+    public UserJpaEntity() {
+    }
+
+    public UserJpaEntity(UUID id, String email, String password, Set<RoleJpaEntity> roles, boolean enabled) {
+        this.id = id;
+        this.email = email;
+        this.password = password;
+        this.roles = roles;
+        this.enabled = enabled;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<RoleJpaEntity> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<RoleJpaEntity> roles) {
+        this.roles = roles;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.enabled = enabled;
+    }
+}
