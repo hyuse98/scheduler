6d701ff34a8d43f04c393539bae26b805999d333
Franklin <franklinpereira98@hotmail.com>
2026-01-16 20:44:24 -0300

    Add Schedule Jpa Entity

---
+++ src/main/java/com/hyuse98/scheduler/core/infrastructure/persistance/jpa/entities/ScheduleJpaEntity.java
@@ -0,0 +1,92 @@
+package com.hyuse98.scheduler.core.infrastructure.persistance.jpa.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+import java.util.Date;
+import java.util.UUID;
+
+@Entity
+@Table(name = "schedule", schema = "core")
+public class ScheduleJpaEntity {
+    @Id
+    @Column(name = "id", nullable = false)
+    private UUID id;
+
+    @Column(name = "client_id")
+    private UUID clientId;
+
+    @Column(name = "service_provider_id")
+    private UUID serviceProviderId;
+
+    @Column(name = "service_type")
+    private String serviceType;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "scheduled_at")
+    private Date scheduledAt;
+
+    @Column(name = "status")
+    private String status;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public UUID getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(UUID clientId) {
+        this.clientId = clientId;
+    }
+
+    public UUID getServiceProviderId() {
+        return serviceProviderId;
+    }
+
+    public void setServiceProviderId(UUID serviceProviderId) {
+        this.serviceProviderId = serviceProviderId;
+    }
+
+    public String getServiceType() {
+        return serviceType;
+    }
+
+    public void setServiceType(String serviceType) {
+        this.serviceType = serviceType;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getScheduledAt() {
+        return scheduledAt;
+    }
+
+    public void setScheduledAt(Date scheduledAt) {
+        this.scheduledAt = scheduledAt;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+}
