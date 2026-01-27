1111cd8b3e962d16287e74105d02ebeb1a23ef5b
Franklin <franklinpereira98@hotmail.com>
2026-01-26 21:23:13 -0300

    ADD scheduling annotation for cron jobs

--- src/main/java/com/hyuse98/scheduler/SchedulerApplication.java
+++ src/main/java/com/hyuse98/scheduler/SchedulerApplication.java
@@ -2,7 +2,9 @@ package com.hyuse98.scheduler;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class SchedulerApplication {
 
