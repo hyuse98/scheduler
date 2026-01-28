6c3c3ddc9addce65e4118c93058748fd99db2f61
Franklin <franklinpereira98@hotmail.com>
2026-01-27 21:53:46 -0300

    Improved logic for SaveClientUseCaseImpl

--- src/main/java/com/hyuse98/scheduler/core/application/usecases/client/SaveClientUseCaseImpl.java
+++ src/main/java/com/hyuse98/scheduler/core/application/usecases/client/SaveClientUseCaseImpl.java
@@ -1,10 +1,13 @@
 package com.hyuse98.scheduler.core.application.usecases.client;
 
-import com.hyuse98.scheduler.core.ClientRequestEvent;
+import com.hyuse98.scheduler.core.ClientRegisteredEvent;
 import com.hyuse98.scheduler.core.application.dto.ClientDto;
 import com.hyuse98.scheduler.core.domain.model.Client;
 import com.hyuse98.scheduler.core.domain.repository.ClientRepository;
 import com.hyuse98.scheduler.core.infrastructure.persistance.jpa.mapper.ClientEntityMapper;
+import jakarta.persistence.EntityExistsException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationEventPublisher;
 
 public class SaveClientUseCaseImpl implements SaveClientUseCase {
@@ -12,6 +15,7 @@ public class SaveClientUseCaseImpl implements SaveClientUseCase {
     private final ClientRepository clientRepository;
     private final ApplicationEventPublisher eventPublisher;
     private final ClientEntityMapper clientEntityMapper;
+    private static final Logger LOG = LoggerFactory.getLogger(SaveClientUseCaseImpl.class);
 
 
     public SaveClientUseCaseImpl(ClientRepository clientRepository, ApplicationEventPublisher eventPublisher, ClientEntityMapper clientEntityMapper) {
@@ -23,14 +27,29 @@ public class SaveClientUseCaseImpl implements SaveClientUseCase {
     @Override
     public void execute(ClientDto clientDto) {
 
+        if (clientRepository.existsById(clientDto.id())) {
+            LOG.warn("Client with id {} already exists", clientDto.id());
+            return;
+        }
+
+        if (clientRepository.findByEmail(clientDto.email()).isPresent()) {
+            throw new EntityExistsException("Email already exists: " + clientDto.email());
+        }
+
         Client newClient = clientEntityMapper.toDomain(clientDto);
 
-        clientRepository.save(newClient);
+        Client savedClient = clientRepository.save(newClient);
 
-        var event = new ClientRequestEvent(
-                //Todo(Create event template do publish)
-        );
-
-        eventPublisher.publishEvent(event);
+        eventPublisher.publishEvent(new ClientRegisteredEvent(
+                savedClient.getId(),
+                savedClient.getEmail(),
+                savedClient.getName(),
+                savedClient.getPhoneNumber(),
+                savedClient.getBirthday(),
+                savedClient.getAddress(),
+                savedClient.getCns(),
+                savedClient.getCreatedAt(),
+                savedClient.getActive()
+        ));
     }
 }
