d151578b320f5641d92b02cd8fafb56731884925
Franklin <franklinpereira98@hotmail.com>
2026-01-28 21:54:10 -0300

    Updated Interface Contracts

--- src/main/java/com/hyuse98/scheduler/core/domain/repository/ClientRepository.java
+++ src/main/java/com/hyuse98/scheduler/core/domain/repository/ClientRepository.java
@@ -3,10 +3,15 @@ package com.hyuse98.scheduler.core.domain.repository;
 import com.hyuse98.scheduler.core.domain.model.Client;
 
 import java.util.Optional;
+import java.util.UUID;
 
 public interface ClientRepository {
 
-    void save (Client client);
+    Client save (Client client);
+
+    boolean existsById (UUID id);
 
     Optional<Client> findByName(String name);
+
+    Optional<Client> findByEmail(String email);
 }
