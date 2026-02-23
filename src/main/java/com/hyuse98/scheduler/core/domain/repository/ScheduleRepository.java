7135ef14e8535c7facd39860cabcbc7754d27652
Franklin <franklinpereira98@hotmail.com>
2026-02-23 02:20:19 -0300

    Created Schedule repository

--- src/main/java/com/hyuse98/scheduler/core/domain/repository/ScheduleRepository.java
+++ src/main/java/com/hyuse98/scheduler/core/domain/repository/ScheduleRepository.java
@@ -1,4 +1,20 @@
 package com.hyuse98.scheduler.core.domain.repository;
 
+import com.hyuse98.scheduler.core.domain.model.Schedule;
+
+import java.util.Collection;
+import java.util.Optional;
+import java.util.UUID;
+
 public interface ScheduleRepository {
+
+    Schedule save(Schedule schedule);
+
+    Optional<Schedule> findById(UUID id);
+
+    Collection<Schedule> findByClientId(UUID clientId);
+
+    Collection<Schedule> findByServiceProviderId(UUID serviceProviderId);
+
+    void deleteById(UUID id);
 }
