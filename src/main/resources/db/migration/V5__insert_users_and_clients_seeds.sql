f3fb0fb006b99cf7cbae4ff784ef4a4f2d71ecc0
franklin <franklinpereira98@hotmail.com>
2026-03-02 14:48:34 -0300

    Add new DB seeds

---
+++ src/main/resources/db/migration/V5__insert_users_and_clients_seeds.sql
@@ -0,0 +1,127 @@
+-- Todos possuem a senha 'Senha123456789' (Bcrypt: $2a$10$n0VDaqDwzmKE3rHrxEvQf.2Vj8aKNbGNKSMR5YWVTbUaPpKgb8qLW)
+INSERT INTO iam.users (id, email, password, enabled)
+VALUES ('00000000-0000-0000-0000-000000000000', 'admin@clinica.com',
+        '$2a$10$n0VDaqDwzmKE3rHrxEvQf.2Vj8aKNbGNKSMR5YWVTbUaPpKgb8qLW',
+        true),
+       ('00000000-0000-0000-0000-000000000001', 'provider@clinica.com',
+        '$2a$10$n0VDaqDwzmKE3rHrxEvQf.2Vj8aKNbGNKSMR5YWVTbUaPpKgb8qLW',
+        true),
+       ('11111111-0000-0000-0000-000000000001', 'joao.silva@email.com',
+        '$2a$10$n0VDaqDwzmKE3rHrxEvQf.2Vj8aKNbGNKSMR5YWVTbUaPpKgb8qLW',
+        true),
+       ('11111111-0000-0000-0000-000000000002', 'ana.clara@email.com',
+        '$2a$10$n0VDaqDwzmKE3rHrxEvQf.2Vj8aKNbGNKSMR5YWVTbUaPpKgb8qLW',
+        true),
+       ('11111111-0000-0000-0000-000000000003', 'pedro.henrique@email.com',
+        '$2a$10$n0VDaqDwzmKE3rHrxEvQf.2Vj8aKNbGNKSMR5YWVTbUaPpKgb8qLW',
+        true),
+       ('11111111-0000-0000-0000-000000000004', 'beatriz.costa@email.com',
+        '$2a$10$n0VDaqDwzmKE3rHrxEvQf.2Vj8aKNbGNKSMR5YWVTbUaPpKgb8qLW',
+        true),
+       ('11111111-0000-0000-0000-000000000005', 'lucas.almeida@email.com',
+        '$2a$10$n0VDaqDwzmKE3rHrxEvQf.2Vj8aKNbGNKSMR5YWVTbUaPpKgb8qLW',
+        true),
+       ('11111111-0000-0000-0000-000000000006', 'mariana.santos@email.com',
+        '$2a$10$n0VDaqDwzmKE3rHrxEvQf.2Vj8aKNbGNKSMR5YWVTbUaPpKgb8qLW',
+        true),
+       ('11111111-0000-0000-0000-000000000007', 'gabriel.oliveira@email.com',
+        '$2a$10$n0VDaqDwzmKE3rHrxEvQf.2Vj8aKNbGNKSMR5YWVTbUaPpKgb8qLW',
+        true),
+       ('11111111-0000-0000-0000-000000000008', 'julia.pereira@email.com',
+        '$2a$10$n0VDaqDwzmKE3rHrxEvQf.2Vj8aKNbGNKSMR5YWVTbUaPpKgb8qLW',
+        true),
+       ('11111111-0000-0000-0000-000000000009', 'matheus.rodrigues@email.com',
+        '$2a$10$n0VDaqDwzmKE3rHrxEvQf.2Vj8aKNbGNKSMR5YWVTbUaPpKgb8qLW',
+        true),
+       ('11111111-0000-0000-0000-000000000010', 'laura.ferreira@email.com',
+        '$2a$10$n0VDaqDwzmKE3rHrxEvQf.2Vj8aKNbGNKSMR5YWVTbUaPpKgb8qLW',
+        true),
+       ('11111111-0000-0000-0000-000000000011', 'rafael.gomes@email.com',
+        '$2a$10$n0VDaqDwzmKE3rHrxEvQf.2Vj8aKNbGNKSMR5YWVTbUaPpKgb8qLW',
+        true),
+       ('11111111-0000-0000-0000-000000000012', 'isabella.martins@email.com',
+        '$2a$10$n0VDaqDwzmKE3rHrxEvQf.2Vj8aKNbGNKSMR5YWVTbUaPpKgb8qLW',
+        true),
+       ('11111111-0000-0000-0000-000000000013', 'felipe.rocha@email.com',
+        '$2a$10$n0VDaqDwzmKE3rHrxEvQf.2Vj8aKNbGNKSMR5YWVTbUaPpKgb8qLW',
+        true),
+       ('11111111-0000-0000-0000-000000000014', 'camila.ribeiro@email.com',
+        '$2a$10$n0VDaqDwzmKE3rHrxEvQf.2Vj8aKNbGNKSMR5YWVTbUaPpKgb8qLW',
+        true),
+       ('11111111-0000-0000-0000-000000000015', 'thiago.carvalho@email.com',
+        '$2a$10$n0VDaqDwzmKE3rHrxEvQf.2Vj8aKNbGNKSMR5YWVTbUaPpKgb8qLW',
+        true),
+       ('11111111-0000-0000-0000-000000000016', 'fernanda.melo@email.com',
+        '$2a$10$n0VDaqDwzmKE3rHrxEvQf.2Vj8aKNbGNKSMR5YWVTbUaPpKgb8qLW',
+        true),
+       ('11111111-0000-0000-0000-000000000017', 'bruno.barbosa@email.com',
+        '$2a$10$n0VDaqDwzmKE3rHrxEvQf.2Vj8aKNbGNKSMR5YWVTbUaPpKgb8qLW',
+        true),
+       ('11111111-0000-0000-0000-000000000018', 'amanda.pinto@email.com',
+        '$2a$10$n0VDaqDwzmKE3rHrxEvQf.2Vj8aKNbGNKSMR5YWVTbUaPpKgb8qLW',
+        true),
+       ('11111111-0000-0000-0000-000000000019', 'diego.mendes@email.com',
+        '$2a$10$n0VDaqDwzmKE3rHrxEvQf.2Vj8aKNbGNKSMR5YWVTbUaPpKgb8qLW',
+        true)
+ON CONFLICT (id) DO NOTHING;
+
+INSERT INTO iam.user_roles (user_id, role_id)
+SELECT '00000000-0000-0000-0000-000000000000', id
+FROM iam.roles
+WHERE name = 'ROLE_ADMIN'
+ON CONFLICT DO NOTHING;
+
+INSERT INTO iam.user_roles (user_id, role_id)
+SELECT '00000000-0000-0000-0000-000000000001', id
+FROM iam.roles
+WHERE name = 'ROLE_SERVICE_PROVIDER'
+ON CONFLICT DO NOTHING;
+
+INSERT INTO iam.user_roles (user_id, role_id)
+SELECT u.id, r.id
+FROM iam.users u
+         CROSS JOIN iam.roles r
+WHERE r.name = 'ROLE_USER'
+  AND u.email != 'admin@clinica.com' AND  u.email != 'provider@clinica.com'
+ON CONFLICT DO NOTHING;
+
+INSERT INTO core.client (id, email, name, phone_number, birthday, address, cns, created_at, is_active)
+VALUES ('11111111-0000-0000-0000-000000000001', 'joao.silva@email.com', 'João Silva', '11999999901', '1980-01-15',
+        'Rua A, 100', '123456789012341', CURRENT_TIMESTAMP, true),
+       ('11111111-0000-0000-0000-000000000002', 'ana.clara@email.com', 'Ana Clara', '11999999902', '1985-02-20',
+        'Rua B, 200', '123456789012342', CURRENT_TIMESTAMP, true),
+       ('11111111-0000-0000-0000-000000000003', 'pedro.henrique@email.com', 'Pedro Henrique', '11999999903',
+        '1990-03-25', 'Rua C, 300', '123456789012343', CURRENT_TIMESTAMP, true),
+       ('11111111-0000-0000-0000-000000000004', 'beatriz.costa@email.com', 'Beatriz Costa', '11999999904', '1992-04-10',
+        'Rua D, 400', '123456789012344', CURRENT_TIMESTAMP, true),
+       ('11111111-0000-0000-0000-000000000005', 'lucas.almeida@email.com', 'Lucas Almeida', '11999999905', '1988-05-05',
+        'Rua E, 500', '123456789012345', CURRENT_TIMESTAMP, true),
+       ('11111111-0000-0000-0000-000000000006', 'mariana.santos@email.com', 'Mariana Santos', '11999999906',
+        '1995-06-12', 'Rua F, 600', '123456789012346', CURRENT_TIMESTAMP, true),
+       ('11111111-0000-0000-0000-000000000007', 'gabriel.oliveira@email.com', 'Gabriel Oliveira', '11999999907',
+        '1983-07-30', 'Rua G, 700', '123456789012347', CURRENT_TIMESTAMP, true),
+       ('11111111-0000-0000-0000-000000000008', 'julia.pereira@email.com', 'Julia Pereira', '11999999908', '1998-08-22',
+        'Rua H, 800', '123456789012348', CURRENT_TIMESTAMP, true),
+       ('11111111-0000-0000-0000-000000000009', 'matheus.rodrigues@email.com', 'Matheus Rodrigues', '11999999909',
+        '2000-09-15', 'Rua I, 900', '123456789012349', CURRENT_TIMESTAMP, true),
+       ('11111111-0000-0000-0000-000000000010', 'laura.ferreira@email.com', 'Laura Ferreira', '11999999910',
+        '1986-10-10', 'Rua J, 1000', '123456789012350', CURRENT_TIMESTAMP, true),
+       ('11111111-0000-0000-0000-000000000011', 'rafael.gomes@email.com', 'Rafael Gomes', '11999999911', '1991-11-05',
+        'Rua K, 1100', '123456789012351', CURRENT_TIMESTAMP, true),
+       ('11111111-0000-0000-0000-000000000012', 'isabella.martins@email.com', 'Isabella Martins', '11999999912',
+        '1994-12-18', 'Rua L, 1200', '123456789012352', CURRENT_TIMESTAMP, true),
+       ('11111111-0000-0000-0000-000000000013', 'felipe.rocha@email.com', 'Felipe Rocha', '11999999913', '1982-01-22',
+        'Rua M, 1300', '123456789012353', CURRENT_TIMESTAMP, true),
+       ('11111111-0000-0000-0000-000000000014', 'camila.ribeiro@email.com', 'Camila Ribeiro', '11999999914',
+        '1989-02-14', 'Rua N, 1400', '123456789012354', CURRENT_TIMESTAMP, true),
+       ('11111111-0000-0000-0000-000000000015', 'thiago.carvalho@email.com', 'Thiago Carvalho', '11999999915',
+        '1997-03-08', 'Rua O, 1500', '123456789012355', CURRENT_TIMESTAMP, true),
+       ('11111111-0000-0000-0000-000000000016', 'fernanda.melo@email.com', 'Fernanda Melo', '11999999916', '1993-04-19',
+        'Rua P, 1600', '123456789012356', CURRENT_TIMESTAMP, true),
+       ('11111111-0000-0000-0000-000000000017', 'bruno.barbosa@email.com', 'Bruno Barbosa', '11999999917', '1987-05-25',
+        'Rua Q, 1700', '123456789012357', CURRENT_TIMESTAMP, true),
+       ('11111111-0000-0000-0000-000000000018', 'amanda.pinto@email.com', 'Amanda Pinto', '11999999918', '1996-06-30',
+        'Rua R, 1800', '123456789012358', CURRENT_TIMESTAMP, true),
+       ('11111111-0000-0000-0000-000000000019', 'diego.mendes@email.com', 'Diego Mendes', '11999999919', '1981-07-07',
+        'Rua S, 1900', '123456789012359', CURRENT_TIMESTAMP, true)
+ON CONFLICT (id) DO NOTHING;
