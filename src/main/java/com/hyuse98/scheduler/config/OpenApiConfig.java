80b519665ff32b88267cf0f955d2c308094a98f9
franklin <franklinpereira98@hotmail.com>
2026-03-03 14:49:08 -0300

    change OpenAPI config

--- src/main/java/com/hyuse98/scheduler/config/OpenApiConfig.java
+++ src/main/java/com/hyuse98/scheduler/config/OpenApiConfig.java
@@ -1,8 +1,11 @@
 package com.hyuse98.scheduler.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,10 +15,17 @@ public class OpenApiConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")))
                 .info(new Info()
-                        .title("Title")
+                        .title("Scheduler API")
                         .version("1.0.0")
-                        .description("Description")
+                        .description("API para gerenciamento de agendamentos e usuários")
                         .contact(new Contact()
                                 .name("Equipe de Arquitetura")
                                 .email("arquitetura@xyz.com")));
