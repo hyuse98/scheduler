b0c7eca505934d2bf1bba6b7e82b8207a053603d
franklin <franklinpereira98@hotmail.com>
2026-03-05 15:02:30 -0300

    Refactoring Usecases

--- src/main/java/com/hyuse98/scheduler/iam/application/usecase/GetUserProfileUsecase.java
+++ src/main/java/com/hyuse98/scheduler/iam/application/usecase/GetUserProfileUsecase.java
@@ -2,8 +2,10 @@ package com.hyuse98.scheduler.iam.application.usecase;
 
 import com.hyuse98.scheduler.iam.application.dto.UserProfileResponse;
 
+import java.util.UUID;
+
 public interface GetUserProfileUsecase {
 
-        UserProfileResponse execute();
+        UserProfileResponse execute(UUID id);
 
 }
