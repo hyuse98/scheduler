4a9d72db72bd35566e41096daf6a169968a7a27a
franklin <franklinpereira98@hotmail.com>
2026-04-06 23:49:17 -0300

    New Queue

--- src/main/java/com/hyuse98/scheduler/config/RabbitMqConfig.java
+++ src/main/java/com/hyuse98/scheduler/config/RabbitMqConfig.java
@@ -13,6 +13,8 @@ public class RabbitMqConfig {
     public static final String USER_REGISTRATION_EXCHANGE = "user.registered";
     public static final String USER_ACTIVE_QUEUE = "core.user.active.queue";
     public static final String USER_ACTIVE_EXCHANGE = "user.active.exchange";
+    public static final String PROVIDER_REGISTRATION_QUEUE = "core.provider.registered.queue";
+    public static final String PROVIDER_REGISTRATION_EXCHANGE = "provider.registered";
 
     @Bean
     public MessageConverter jsonMessageConverter() {
@@ -45,7 +47,23 @@ public class RabbitMqConfig {
     }
 
     @Bean
-    public Binding userActiveBinding(Queue userActiveQueue,  TopicExchange userActiveExchange) {
+    public Binding userActiveBinding(Queue userActiveQueue, TopicExchange userActiveExchange) {
         return BindingBuilder.bind(userActiveQueue).to(userActiveExchange).with("#");
     }
+
+    @Bean
+    public Queue providerRegistrationQueue() {
+        return new Queue(PROVIDER_REGISTRATION_QUEUE, true);
+    }
+
+    @Bean
+    public TopicExchange providerRegistrationExchange() {
+        return new TopicExchange(PROVIDER_REGISTRATION_EXCHANGE);
+    }
+
+    @Bean
+    public Binding providerRegistrationBinding(Queue providerRegistrationQueue, TopicExchange providerRegistrationExchange) {
+        return BindingBuilder.bind(providerRegistrationQueue).to(providerRegistrationExchange).with("#");
+    }
 }
+
