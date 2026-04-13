37baaee66d1db17086922fe6a3803d8f1a01661b
franklin <franklinpereira98@hotmail.com>
2026-04-13 12:31:20 -0300

    Update Documentation

--- src/main/java/com/hyuse98/scheduler/iam/application/events/ServiceProviderRegisteredEvent.java
+++ src/main/java/com/hyuse98/scheduler/iam/application/events/ServiceProviderRegisteredEvent.java
@@ -1,9 +1,12 @@
-package com.hyuse98.scheduler.iam;
+package com.hyuse98.scheduler.iam.application.events;
 
 import org.springframework.modulith.events.Externalized;
 
 import java.util.UUID;
 
+/**
+ * Used by RegisterServiceProviderUseCaseImpl to send event of registration
+ */
 @Externalized("provider.registered")
 public record ServiceProviderRegisteredEvent(
         UUID userId,
