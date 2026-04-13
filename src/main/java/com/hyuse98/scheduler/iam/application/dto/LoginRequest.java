999ad3cc0139a7caa9a643a03601dbdf1ce07160
franklin <franklinpereira98@hotmail.com>
2026-04-13 12:30:41 -0300

    Update Documentation

--- src/main/java/com/hyuse98/scheduler/iam/application/dto/LoginRequest.java
+++ src/main/java/com/hyuse98/scheduler/iam/application/dto/LoginRequest.java
@@ -3,9 +3,9 @@ package com.hyuse98.scheduler.iam.application.dto;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 public record LoginRequest(
-        @Schema(description = "E-mail de acesso do usuário", example = "name@hotmail.com")
+        @Schema(description = "User access email", example = "name@hotmail.com")
         String email,
 
-        @Schema(description = "Senha de acesso", example = "Senha123456789")
+        @Schema(description = "User access password", example = "Senha123456789")
         String password
 ) {}
