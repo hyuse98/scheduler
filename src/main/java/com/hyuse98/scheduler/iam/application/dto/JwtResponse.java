c03007fbc6b0f0b4f652f9bb72c9632caa9d5d88
franklin <franklinpereira98@hotmail.com>
2026-04-13 12:30:36 -0300

    Update Documentation

--- src/main/java/com/hyuse98/scheduler/iam/application/dto/JwtResponse.java
+++ src/main/java/com/hyuse98/scheduler/iam/application/dto/JwtResponse.java
@@ -3,6 +3,6 @@ package com.hyuse98.scheduler.iam.application.dto;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 public record JwtResponse(
-        @Schema(description = "Token JWT gerado para a sessão", example = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...")
+        @Schema(description = "JWT token generated for the session", example = "eyJhbGciOiJIUzM4NCJ9...")
         String token
 ) {}
