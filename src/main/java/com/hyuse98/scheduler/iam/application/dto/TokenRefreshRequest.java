1b986bc21db84b6d429cec047d9d290a52b110a1
franklin <franklinpereira98@hotmail.com>
2026-05-01 22:04:17 -0300

    Add TokenRefreshRequest

---
+++ src/main/java/com/hyuse98/scheduler/iam/application/dto/TokenRefreshRequest.java
@@ -0,0 +1,10 @@
+package com.hyuse98.scheduler.iam.application.dto;
+
+public class TokenRefreshRequest {
+
+    private String refreshToken;
+
+    public String getRefreshToken() {
+        return refreshToken;
+    }
+}
